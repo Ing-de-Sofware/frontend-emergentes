6e6292c99b052da2e17eeba25cdd6a2cdf3eb61d
AngeloC12 <angelocuri@hotmail.com>
2025-10-03 17:43:10 -0500

    feat: add sidenav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {SidenavComponent} from './public/sidenav/sidenav.component';
 
 export const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-
+  { path: 'sidenav', component: SidenavComponent}
 
 ];
